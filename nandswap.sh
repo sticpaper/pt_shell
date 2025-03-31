a06229f86fcc776b61614f7a80d8f36e9c4139f8
stic-server-open <1138705738@qq.com>
2025-03-31 22:00:43 +0800

    [FEATURE]: create nandswap.sh Fix ColorOS Memory Extend
    Change-Id: Icd3bab5c90ba06cb754fe4d96a52fdff5f064404

---
+++ nandswap.sh
@@ -0,0 +1,58 @@
+#! /vendor/bin/sh
+
+# f2fs_io=/system_ext/bin/f2fs_io
+f2fs_io=/data/amktiao/f2fs_io # debug
+oplus_nandswap_stat=persist.sys.oplus.nandswap
+oplus_nandswap_enable=persist.sys.oplus.nandswap.condition
+oplus_nandswap_size=persist.sys.oplus.nandswap.swapsize
+
+function run_oplus_nandswap_ocheck()
+{
+    if [ $(grep -o "zram[0-9]" /proc/swaps) == "zram0" ]; then
+        swapoff /dev/block/zram0
+        echo 1 > /sys/block/zram0/reset
+    fi
+}
+
+function configure_zram_parameters()
+{
+    nandswap_path="/data/nandswap"
+    nandswap_size=$(getprop $oplus_nandswap_size)
+    zram_sysfs="/sys/block/zram0"
+    nandswap_disk=$(losetup -f)
+
+    if [ ! -d $nandswap_path ]; then
+        mkdir /data/nandswap
+    fi
+
+    if [ -f $nandswap_path/swapfile ]; then
+        rm $nandswap_path/swapfile
+    fi
+
+    touch $nandswap_path/swapfile
+    $f2fs_io pinfile set $nandswap_path/swapfile
+    fallocate -o 0 -l ${nandswap_size}G $nandswap_path/swapfile
+    losetup "$nandswap_disk" $nandswap_path/swapfile
+    echo "$nandswap_disk" > $zram_sysfs/backing_dev
+    echo ${nandswap_size}G > $zram_sysfs/disksize
+
+    mkswap /dev/block/zram0
+    swapon /dev/block/zram0 -p 32765
+
+    # set apply oplus_system property
+    setprop $oplus_nandswap_enable true
+}
+
+function oplus_nandswap_config()
+{
+    nandswap_enable=$(getprop $oplus_nandswap_stat)
+
+    if [ $nandswap_enable == "true" ]; then
+        [ ! -f $f2fs_io ] && exit 0
+        run_oplus_nandswap_ocheck
+        configure_zram_parameters
+    fi
+}
+
+# main-nandswap
+oplus_nandswap_config
