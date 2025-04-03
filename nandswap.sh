76f157627233db073318ba1ee491bbae192ab96b
stic-server-open <1138705738@qq.com>
2025-04-03 19:45:31 +0800

    [FEATURE] nandswap: Refact nandswap config zram device bootup logic
    * 1. Remove redundant nandswap config logic (These function unused)
      2. Config zram device size to always match the physical memory size
    
    Change-Id: Ifbc44091a4374e2b21c5d52dd37f319eca11985b

--- nandswap.sh
+++ nandswap.sh
@@ -16,45 +16,46 @@ function run_oplus_nandswap_ocheck()
 
 function configure_zram_parameters()
 {
-    nandswap_path="/data/nandswap"
-    nandswap_size=$(getprop $oplus_nandswap_size)
+    nandswap_enable=$1
     zram_sysfs="/sys/block/zram0"
+    memtotal=$(grep "MemTotal" /proc/meminfo | grep -oE '[0-9]+')
+    zram_disksize=$(expr $memtotal / 1024 / 1024 + 1)
 
-    if [ ! -d $nandswap_path ]; then
-        mkdir /data/nandswap
+    run_oplus_nandswap_ocheck
+    if [ $nandswap_enable == "true" ]; then
+        nandswap_path="/data/nandswap"
+        nandswap_size=$(getprop $oplus_nandswap_size)
+
+        if [ ! -d $nandswap_path ]; then
+            mkdir /data/nandswap
+        fi
+
+        if [ -f $nandswap_path/swapfile ]; then
+            rm $nandswap_path/swapfile
+        fi
+
+        [ ! -f $f2fs_io ] && exit 0
+        if [ ! $(df -h /data | awk 'NR==2 {print $5+0}') -ge 80 ]; then
+            nandswap_disk=$(losetup -f)
+            touch $nandswap_path/swapfile
+            $f2fs_io pinfile set $nandswap_path/swapfile
+            fallocate -o 0 -l ${nandswap_size}G $nandswap_path/swapfile
+            losetup "$nandswap_disk" $nandswap_path/swapfile
+            echo "$nandswap_disk" > $zram_sysfs/backing_dev
+            setprop $oplus_nandswap_enable true
+        fi
     fi
 
-    if [ -f $nandswap_path/swapfile ]; then
-        rm $nandswap_path/swapfile
-    fi
-
-    if [ ! $(df -h /data | awk 'NR==2 {print $5+0}') -ge 80 ]; then
-        nandswap_disk=$(losetup -f)
-        touch $nandswap_path/swapfile
-        $f2fs_io pinfile set $nandswap_path/swapfile
-        fallocate -o 0 -l ${nandswap_size}G $nandswap_path/swapfile
-        losetup "$nandswap_disk" $nandswap_path/swapfile
-        echo "$nandswap_disk" > $zram_sysfs/backing_dev
-    fi
-
-    echo ${nandswap_size}G > $zram_sysfs/disksize
+    echo ${zram_disksize}G > $zram_sysfs/disksize
 
     mkswap /dev/block/zram0
     swapon /dev/block/zram0 -p 32765
-
-    # set apply oplus_system property
-    setprop $oplus_nandswap_enable true
 }
 
 function oplus_nandswap_config()
 {
-    nandswap_enable=$(getprop $oplus_nandswap_stat)
-
-    if [ $nandswap_enable == "true" ]; then
-        [ ! -f $f2fs_io ] && exit 0
-        run_oplus_nandswap_ocheck
-        configure_zram_parameters
-    fi
+    nandswap=$(getprop $oplus_nandswap_stat)
+    configure_zram_parameters $nandswap
 }
 
 # main-nandswap
